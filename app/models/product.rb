ea18899002091be057c9386bd6a2d7f729ff7e2b
bilal-javed <bjaved51@gmail.com>
2015-12-11 20:32:48 +0500

    added paperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :attachments_attributes
 
   has_many :comments
+  has_many :attachments, as: :attachable, dependent: :destroy
+  accepts_nested_attributes_for :attachments
 end
---
+++ db/migrate/20151211143044_add_photo_to_attachments.rb
@@ -0,0 +1,10 @@
+class AddPhotoToAttachments < ActiveRecord::Migration
+
+  def up
+    add_attachment :attachments, :photo
+  end
+
+  def down
+    remove_attachment :attachments, :photo
+  end
+end
